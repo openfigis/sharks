a763afd3ad8c392dc0ea11a5c6c621aa063e15d6
Federico De Faveri <federico.defaveri@fao.org>
2015-03-19 16:48:15 +0100

    Enabled back button

--- sharks-client/app/scripts/controllers/shark_species_details.js
+++ sharks-client/app/scripts/controllers/shark_species_details.js
@@ -13,4 +13,8 @@ angular.module("sharksClient")
 	  this.ids = $routeParams.ids.split(",");
 	  $log.info("ids "+this.ids);
 	  this.ems = measuresservice.groupByEntity({species:this.ids});
+	  
+	  this.back = function() {
+		  $location.path("/species");
+	  }
   }]);
