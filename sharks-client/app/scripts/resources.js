9cc742bdbce440067ac2a70c4c74d61f9dc1072b
Federico De Faveri <federico.defaveri@fao.org>
2015-03-20 08:06:46 +0100

    Enabled resource caching

--- sharks-client/app/scripts/resources.js
+++ sharks-client/app/scripts/resources.js
@@ -3,11 +3,18 @@
 var services = angular.module("resources", ["ngResource","configuration"]);
 
 services.factory("speciesresource", ["$resource", "config", function($resource, config) {
-	return $resource(config.sharksWebUrl + "species/:id");
+	return $resource(config.sharksWebUrl + "species/:id", null, {
+		query: {
+            method: "GET",
+            cache: true, 
+            isArray:true
+        }
+		
+	});
 }]);
 
 services.factory("measuresresource", ["$resource", "config", function($resource, config) {
 	return $resource(config.sharksWebUrl + "measures/:id/", null, {
-		groupByEntity: {method:'GET', url:config.sharksWebUrl + "measures/groupByEntity", isArray:true}
+		groupByEntity: {method:'GET', url:config.sharksWebUrl + "measures/groupByEntity", isArray:true, cache: true}
 	});
 }]);
