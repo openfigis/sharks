b59ae3f4ef18725ac2ba3619f763685e5239636b
Federico De Faveri <federico.defaveri@fao.org>
2015-03-23 17:38:06 +0100

    Added temporary solution to db retrieving

--- sharks-server/sharks-storage/src/main/java/org/sharks/storage/config/Configuration.java
+++ sharks-server/sharks-storage/src/main/java/org/sharks/storage/config/Configuration.java
@@ -3,6 +3,8 @@
  */
 package org.sharks.storage.config;
 
+import java.io.File;
+
 /**
  * TODO should we move this class to a separate project?
  * @author "Federico De Faveri federico.defaveri@fao.org"
@@ -12,10 +14,14 @@ public class Configuration {
 	
 	private static String ENV_PROPERTY = "SHARKS_DB";
 	private static String DEV_DB_LOCATION = "../sharks-db/Sharks.accdb";
+	private static String DEV_DB_LOCATION2 = "db/Sharks.accdb";
 	
 	public String getDbFileLocation() {
+		System.out.println("current location "+(new File(".").getAbsolutePath()));
 		String dbLocation = System.getenv(ENV_PROPERTY);
-		if (dbLocation == null) dbLocation = DEV_DB_LOCATION;
+		//FIXME tmp workaround until we take a decision about configuration
+		if (dbLocation == null && (new File(DEV_DB_LOCATION)).exists()) dbLocation = DEV_DB_LOCATION;
+		if (dbLocation == null && (new File(DEV_DB_LOCATION2)).exists()) dbLocation = DEV_DB_LOCATION2;
 		return dbLocation;
 	}
 	
