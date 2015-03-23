8b56238c4ca37c43473ba63ff5c2316a9302cb79
Federico De Faveri <federico.defaveri@fao.org>
2015-03-23 10:36:10 +0100

    housekeeping

--- sharks-client/app/scripts/services.js
+++ sharks-client/app/scripts/services.js
@@ -45,39 +45,4 @@ services.factory("speciesservice", ["$cacheFactory", "speciesresource", "$log", 
 	}
 	return new SpeciesService();
 	
-	/*{
-		selected : [],
-		
-		toggleSelection : function(species) {
-			var index = this.selected.indexOf(species.code);
-			if (index < 0) this.selected.push(species.code);
-			else this.selected.splice(index, 1);
-		},
-		
-		list : function() {
-			return speciesresource.query();
-		},
-		
-		get : function(code) {
-			var species = speciesresource.query();
-			for (var i = 0; i < species.length; i++) {
-			    if (species[i].code === code) return species[i];
-			}
-			return null;
-		},
-		
-		getAll : function(codes) {
-			var deferred = $q.defer();
-			speciesresource.query().$promise.then(function(species) {
-				var found = [];
-				for (var i = 0; i < species.length; i++) {
-				    if (codes.indexOf(species[i].code) >= 0) found.push(species[i]);
-				}
-				deferred.resolve(found);
-            });
-			
-			return deferred.promise;
-		}
-	}*/
-	
 }]);
