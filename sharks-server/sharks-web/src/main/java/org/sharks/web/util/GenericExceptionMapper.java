9f817a9a0d83d1e4c90208da09a8ec4ab3235ab8
Federico De Faveri <federico.defaveri@fao.org>
2015-03-26 10:14:35 +0100

    Removed weld logger, started using lombok slf4j annotation

--- sharks-server/sharks-web/src/main/java/org/sharks/web/util/GenericExceptionMapper.java
+++ sharks-server/sharks-web/src/main/java/org/sharks/web/util/GenericExceptionMapper.java
@@ -6,14 +6,17 @@ package org.sharks.web.util;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author "Federico De Faveri federico.defaveri@fao.org"
  *
  */
+@Slf4j
 public class GenericExceptionMapper implements ExceptionMapper<Exception> {
 	
 	public Response toResponse(Exception ex) {
-		ex.printStackTrace();
+		log.error("Exception", ex);
 		return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())
 				.build();
 	}
