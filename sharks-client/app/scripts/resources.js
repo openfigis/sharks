d514501d1060e2668eaa75db4d02e41828ea6c87
Federico De Faveri <federico.defaveri@fao.org>
2015-03-27 12:10:03 +0100

    Added countries and management entities resources

--- sharks-client/app/scripts/resources.js
+++ sharks-client/app/scripts/resources.js
@@ -4,17 +4,24 @@ var services = angular.module("resources", ["ngResource","config"]);
 
 services.factory("speciesresource", ["$resource", "rest", function($resource, rest) {
 	return $resource(rest.baseUrl + "species/:id", null, {
-		query: {
-            method: "GET",
-            cache: true, 
-            isArray:true
-        }
-		
+		query: {method: "GET", cache: true, isArray:true}
 	});
 }]);
 
 services.factory("measuresresource", ["$resource", "rest", function($resource, rest) {
 	return $resource(rest.baseUrl + "measures/:id/", null, {
 		groupByEntity: {method:"GET", url:rest.baseUrl + "measures/groupByEntity", isArray:true, cache: true}
+	});
+}]);
+
+services.factory("countriesresource", ["$resource", "rest", function($resource, rest) {
+	return $resource(rest.baseUrl + "countries/:id/", null, {
+		query: {method: "GET", cache: true, isArray:true}
+	});
+}]);
+
+services.factory("mgmentitiesresource", ["$resource", "rest", function($resource, rest) {
+	return $resource(rest.baseUrl + "managemententities/:id/", null, {
+		query: {method: "GET", cache: true, isArray:true}
 	});
 }]);
