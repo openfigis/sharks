cfdebfd244239b5a5cccddf30b7b6842cf9ef349
Federico De Faveri <federico.defaveri@fao.org>
2015-04-28 13:14:07 +0200

    Species visualized as images

--- sharks-client/app/scripts/controllers/species_selection.js
+++ sharks-client/app/scripts/controllers/species_selection.js
@@ -13,4 +13,9 @@ angular.module("sharksClient")
 	  this.show = function(species) {
 		  routingservice.toSingle("species", species);
 	  };
+	  
+	  this.imageUrl = function(species) {
+		  var sn = species.scientificName.toLowerCase().replace(" ", "_");
+		  return "images/species/"+sn+"-drawing-small.gif";  
+	  };
   }]);
