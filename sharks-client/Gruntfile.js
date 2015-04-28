633b6f4d762672ff0adcf8d8f0b8847245463949
Federico De Faveri <federico.defaveri@fao.org>
2015-04-28 13:44:35 +0200

    Species images not more renamed

--- sharks-client/Gruntfile.js
+++ sharks-client/Gruntfile.js
@@ -231,6 +231,7 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '!<%= yeoman.dist %>/images/species/**',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
