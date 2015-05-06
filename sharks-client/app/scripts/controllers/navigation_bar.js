e13d0fec7fd02d23a6347360feb024f5d2514cdf
Federico De Faveri <federico.defaveri@fao.org>
2015-05-06 17:55:30 +0200

    Fixed tab url calculation

--- sharks-client/app/scripts/controllers/navigation_bar.js
+++ sharks-client/app/scripts/controllers/navigation_bar.js
@@ -5,6 +5,10 @@ angular.module("sharksClient")
                                          function ($location, paths) {
 	  this.paths = paths;
 
+	  this.href = function(path) { 
+	  	return path.all.substring(1, path.all.length);
+	  };
+	  
 	  this.isActive = function(path) { 
 	  	return $location.path().startsWith(path.all) || $location.path().startsWith(path.singlePath);
 	  };
