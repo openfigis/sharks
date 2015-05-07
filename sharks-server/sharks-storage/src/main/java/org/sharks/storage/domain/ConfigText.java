e8bfb44aa08f601df24fdff4d170658068aa53b9
Federico De Faveri <federico.defaveri@fao.org>
2015-05-07 08:25:56 +0200

    Updated database and model

---
+++ sharks-server/sharks-storage/src/main/java/org/sharks/storage/domain/ConfigText.java
@@ -0,0 +1,30 @@
+
+package org.sharks.storage.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+/**
+ * @author "Federico De Faveri federico.defaveri@fao.org"
+ * 
+ */
+@Data
+@Entity(name = "tbConfigText")
+@XmlRootElement
+@EqualsAndHashCode(of = "code")
+public class ConfigText {
+
+    @Id
+    @Column(name = "cdConfigText")
+    private Long code;
+    @Column
+    private String cdMnemonicKey;
+    @Column
+    private String description;
+
+}
