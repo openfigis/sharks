7f4cd1336fa39a583baebb2c6e855745bdd7e6e7
Federico De Faveri <federico.defaveri@fao.org>
2015-05-18 12:15:11 +0200

    Styled group details page

--- sharks-client/app/scripts/controllers/group_details.js
+++ sharks-client/app/scripts/controllers/group_details.js
@@ -12,9 +12,24 @@ angular.module("sharksClient")
                                           function (routingservice, imagesservice, group) {
 	  
 	  this.group = group;
-	  this.ems = Stream(group.measures).groupBy(function (measure) {
+	  this.ems = Stream(group.measures)
+	  .filter(function (measure) {
+		  return measure.entityAcronym !== "CITES" && measure.entityAcronym !== "CMS";
+	  })
+	  .groupBy(function (measure) {
 	      return measure.entityAcronym;
 	  });
+	  
+	  this.showCitesLink = Stream(group.measures).anyMatch({entityAcronym:"CITES"});
+	  this.showCmsLink = Stream(group.measures).anyMatch({entityAcronym:"CMS"});
+	  
+	  this.showEntity = function(acronym) {
+		  routingservice.toSingleById("entities",acronym);
+	  };
+	  
+	  this.showSpecies = function(species) {
+		  routingservice.toSingle("species", species);
+	  };
 	  
 	  this.imageUrl = imagesservice.groupMediumImageUrl;
 	  this.missingImageUrl = imagesservice.groupMissingMediumImageUrl;
