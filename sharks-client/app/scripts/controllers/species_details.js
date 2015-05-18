165b35ee958650c552eeccc5d5cb762572e28fcd
Federico De Faveri <federico.defaveri@fao.org>
2015-05-18 11:33:40 +0200

    Added factsheets url in species details

--- sharks-client/app/scripts/controllers/species_details.js
+++ sharks-client/app/scripts/controllers/species_details.js
@@ -8,20 +8,22 @@
  * Controller of the sharksClient
  */
 angular.module("sharksClient")
-  .controller("SpeciesDetailsCtrl", ["routingservice", "imagesservice", "species",  
-                                          function (routingservice, imagesservice, species) {
+  .controller("SpeciesDetailsCtrl", ["routingservice", "imagesservice", "factsheets", "species",  
+                                          function (routingservice, imagesservice, factsheets, species) {
 	  
 	  this.species = species;
 	  this.ems = Stream(species.measures)
-	  .filter(function (measure) {
-		  return measure.entityAcronym !== "CITES" && measure.entityAcronym !== "CMS";
-	  })
-	  .groupBy(function (measure) {
-	      return measure.entityAcronym;
-	  });
+		  .filter(function (measure) {
+			  return measure.entityAcronym !== "CITES" && measure.entityAcronym !== "CMS";
+		  })
+		  .groupBy(function (measure) {
+		      return measure.entityAcronym;
+		  });
 	  
 	  this.showCitesLink = Stream(species.measures).anyMatch({entityAcronym:"CITES"});
 	  this.showCmsLink = Stream(species.measures).anyMatch({entityAcronym:"CMS"});
+	  
+	  this.factsheetsUrl = species.figisId !== null? factsheets.baseUrl + species.figisId : null;
 	  
 	  this.showEntity = function(acronym) {
 		  routingservice.toSingleById("entities",acronym);
