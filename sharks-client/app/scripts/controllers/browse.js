c63e0b710f8d6b49c13fa1c1879422d64288a9c2
Federico De Faveri <federico.defaveri@fao.org>
2015-05-19 08:58:56 +0200

    Synched browse, now items sorted

--- sharks-client/app/scripts/controllers/browse.js
+++ sharks-client/app/scripts/controllers/browse.js
@@ -3,10 +3,11 @@
 angular.module("sharksClient")
 .controller("BrowseCtrl", ["routingservice", "species", "groups", "entities", "countries",
                                      function (routingservice, species, groups, entities, countries) {
-	  this.species = species;
-	  this.groups = groups;
-	  this.entities = entities;
-	  this.countries = countries;
+	
+	  this.species = Stream(species).sorted("englishName").toArray();
+	  this.groups = Stream(groups).sorted("name").toArray();
+	  this.entities = Stream(entities).sorted("acronym").toArray();
+	  this.countries = Stream(countries).sorted("name").toArray();
 	  
 	  this.showSpecies = function(species) {
 		  routingservice.toSingle("species", species);
