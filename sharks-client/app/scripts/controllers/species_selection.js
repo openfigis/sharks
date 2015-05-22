5685a0d2ad08dae95236f0251d0c502b0b096d9b
Federico De Faveri <federico.defaveri@fao.org>
2015-05-22 10:46:49 +0200

    Species selection demo.

--- sharks-client/app/scripts/controllers/species_selection.js
+++ sharks-client/app/scripts/controllers/species_selection.js
@@ -8,16 +8,42 @@
  * Controller of the sharksClient
  */
 angular.module("sharksClient")
-  .controller("SpeciesSelectionCtrl", ["routingservice", "imagesservice", "pageservice", 
+  .controller("SpeciesSelectionCtrl", ["routingservice", "imagesservice", "pageservice", "$timeout", "$scope", 
                                        "species", "groups", "footer", "isHome",
-                                       function (routingservice, imagesservice, pageservice,
+                                       function (routingservice, imagesservice, pageservice, $timeout, $scope, 
                                     		   species, groups, footer, isHome) {
+	  
+	  var self = this;
 	  
 	  this.species = Stream(species).sorted("englishName").toArray();
 	  this.groups = Stream(groups).sorted("name").toArray();
 
 	  this.title = "";
 	  this.subTitle = "";
+	  
+	  this.speciesIterator = Stream(groups).iterator();
+	  this.onAnimationTimeout = function(){
+		  var nextElement = self.speciesIterator.next();
+		  var value = nextElement.value;
+		  var isSpecies = value.scientificName!==undefined;
+		  
+		  if (isSpecies) self.hoverSpecies(value);
+		  else self.hoverGroup(value);
+		  
+		  if (nextElement.done) self.speciesIterator = Stream(isSpecies?groups:species).iterator();
+		  
+		  self.animationTimeout = $timeout(self.onAnimationTimeout, 1250);
+	  };
+	  
+	  this.animationTimeout = $timeout(self.onAnimationTimeout, 1250);
+	    
+	  this.stopAnimation = function(){
+		  $timeout.cancel(this.animationTimeout);
+	  };
+	  
+	  $scope.$on("$destroy", function(){
+		  self.stopAnimation();
+	  });
 	  
 	  if (isHome) pageservice.setHomeTitle();
 	  else pageservice.setTitle("species");
