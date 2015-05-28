0722e77cadeb724727007b1e4ef5854cde623f8c
erikvaningen <erikvaningen@gmail.com>
2015-05-28 10:10:12 +0200

    correcting group id

--- sharks-server/sharks-storage/lib/2repo.bat
+++ sharks-server/sharks-storage/lib/2repo.bat
@@ -1,3 +1,3 @@
 mvn install:install-file -Dfile=ucanaccess-2.0.9.3.jar -DgroupId=net.ucanaccess -DartifactId=ucanaccess-2.0.9.3.jar -Dversion=2.0.9.3 -Dpackaging=jar
-mvn install:install-file -Dfile=jackcess-2.0.8.jar -DgroupId=jackcess -DartifactId=jackcess -Dversion=2.0.8 -Dpackaging=jar
+mvn install:install-file -Dfile=jackcess-2.0.8.jar -DgroupId=com.healthmarketscience.jackcess -DartifactId=jackcess -Dversion=2.0.8 -Dpackaging=jar
 
