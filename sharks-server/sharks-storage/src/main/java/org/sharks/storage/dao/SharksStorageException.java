fd5152580637800c5ac4e71680b2ac73d283e095
erikvaningen <erikvaningen@gmail.com>
2015-06-04 16:52:43 +0200

    update doc

--- sharks-server/sharks-storage/src/main/java/org/sharks/storage/dao/SharksStorageException.java
+++ sharks-server/sharks-storage/src/main/java/org/sharks/storage/dao/SharksStorageException.java
@@ -1,14 +1,15 @@
 package org.sharks.storage.dao;
 
 /**
- * Generic vme runtime exception. Vme so far does not have checked exceptions, the idea behind is that checked
- * exceptions create a lot of ugly boilerplate code, and more important, all code should be tested so that exception
- * will not occur runtime.
+ * Generic SharksStorageException runtime exception. Vme so far does not have checked exceptions, the idea behind is
+ * that checked exceptions create a lot of ugly boilerplate code, and more important, all code should be tested so that
+ * exception will not occur runtime.
+ * 
  * @author Erik van Ingen
  * 
  */
 public class SharksStorageException extends RuntimeException {
-	
+
 	private static final long serialVersionUID = 1359907165992410885L;
 
 	public SharksStorageException(String message) {
