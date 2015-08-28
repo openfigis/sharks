2adc2eae159db007b9d2780a32975f17eb51a305
Federico De Faveri <federico.defaveri@fao.org>
2015-08-28 12:53:47 +0200

    Removed partials to increase performance

--- sharks-client/app/scripts/controllers/loader.js
+++ sharks-client/app/scripts/controllers/loader.js
@@ -1,24 +1,21 @@
 "use strict";
 
 angular.module("sharksClient")
-.directive("loader", function() {
-  return {
-	  restrict: "E",
-	  templateUrl: "partials/loader.html",
-	  link: function(scope, element) {
-	      // Store original display mode of element
-	      var shownType = element.css("display");
-	      function hideElement() {
-	        element.css("display", "none");
-	      }
-	            
-	      scope.$on("$routeChangeStart", function() {
-	        element.css("display", shownType);
-	      });
-	      scope.$on("$routeChangeSuccess",hideElement);
-	      scope.$on("$routeChangeError", hideElement);
-	      // Initially element is hidden
-	      hideElement();
-	    }
-  };
-});
+.controller("LoaderCtrl", ["$scope", function ($scope) {
+	var self = this;
+	this.showLoader = false; 
+	
+    function hideLoader() {
+    	self.showLoader = false;
+    }
+    
+    $scope.$on("$routeChangeStart", function() {
+    	self.showLoader = true;
+    });
+    
+    $scope.$on("$routeChangeSuccess",hideLoader);
+    $scope.$on("$routeChangeError", hideLoader);
+      
+    // Initially element is hidden
+    hideLoader();
+}]);
