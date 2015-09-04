df4a24ca58c530fdf3a2c268a5e9e720823a498b
Federico De Faveri <federico.defaveri@fao.org>
2015-09-04 12:28:00 +0200

    Added IE js fix

---
+++ sharks-client/app/scripts/polyfill.js
@@ -0,0 +1,8 @@
+"use strict";
+
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.indexOf(searchString, position) === position;
+  };
+}
