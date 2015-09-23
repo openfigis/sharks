66d3deeb172541e8f1786c2154c8ed40d5c8643e
erikvaningen <erikvaningen@gmail.com>
2015-09-23 15:48:49 +0200

    updating the meta tag for the HTML, for SEO reasons

--- sharks-client/app/scripts/controllers/country_details.js
+++ sharks-client/app/scripts/controllers/country_details.js
@@ -1,67 +1,82 @@
 "use strict";
 
-angular.module("sharksClient")
-  .controller("CountryDetailsCtrl", ["$filter", "routingservice", "pageservice", "imagesservice", "countryprofiles", "faolex", "country",
-                                          function ($filter, routingservice, pageservice, imagesservice, countryprofiles, faolex, country) {
-	  var self = this;
-	  this.country = country;
-	  
-	  pageservice.setTitle(country.name);
-	  pageservice.setDescription("PoAs for country "+country.name);
-	  
-	  
-	  this.groupedPoas = Stream(country.poas)
-	  		.sort(function(a, b) {
-	  			if (a.poAYear === b.poAYear) return 0;
-	  			if (a.poAYear > b.poAYear) return -1;
-	  			return 1;
-	  		})
-	  		.groupBy(function (poa) {
-	  			return poa.type;
-	  });
-	  
-	  this.others = Stream(country.others)
-	  		.sort(function(a, b) {
-			    if (a.year === b.year) return 0;
-			    if (a.year > b.year) return -1;
-			    return 1;
-			 })
-			 .toArray();
-	  
-	  this.dateFormat = "dd MMMM yyyy";
-	  
-	  this.getYearTitle = function(doc) {
-		  return doc.dateOfText!==null?"Date of text: " + $filter("date")(doc.dateOfText, self.dateFormat):
-			  doc.dateOfOriginalText!==null?"Date of original text: " + $filter("date")(doc.dateOfOriginalText, self.dateFormat):null;
-	  };
-	  
-	  this.getYear = function(doc) {
-		  return doc.dateOfText!==null?doc.dateOfText:
-			  doc.dateOfOriginalText!==null?doc.dateOfOriginalText:null;
-	  };
-	  
-	  this.docs = Stream(country.faoLexDocuments)
-		.sort(function(a, b) {
-			var aYear = self.getYear(a);
-			var bYear = self.getYear(b);
-		    if (aYear === bYear) return 0;
-		    if (aYear > bYear) return -1;
-		    return 1;
-		 })
-		 .toArray();
-	  
-	  this.flagUrl = imagesservice.countryFlagUrl(country);
-	  this.noFlagUrl = imagesservice.missingFlagUrl;
-	  this.profileUrl = countryprofiles.profileBaseUrl+country.code+"/en";
-	  this.faoLexUrl = faolex.baseUrl + (country.code === "EUR" ?"EC:":"ISO:") + country.code;
-	  
-	  this.showPoa = function(poa) {
-		routingservice.toSingle("poas", poa);  
-	  };
-	  
-	  
-	  this.showEntityById = function(acronym) {
-		  routingservice.toSingleById("entities", acronym);
-	  };
+angular
+		.module("sharksClient")
+		.controller(
+				"CountryDetailsCtrl",
+				[
+						"$filter",
+						"routingservice",
+						"pageservice",
+						"imagesservice",
+						"countryprofiles",
+						"faolex",
+						"country",
+						function($filter, routingservice, pageservice, imagesservice, countryprofiles, faolex,
+								country) {
+							var self = this;
+							this.country = country;
 
-  }]);
+							pageservice.setTitle(country.name);
+							pageservice
+									.setDescription("List of "
+											+ country.name
+											+ "National Plan of Action (NPOA), relevant documents and related national legislation.");
+
+							this.groupedPoas = Stream(country.poas).sort(function(a, b) {
+								if (a.poAYear === b.poAYear)
+									return 0;
+								if (a.poAYear > b.poAYear)
+									return -1;
+								return 1;
+							}).groupBy(function(poa) {
+								return poa.type;
+							});
+
+							this.others = Stream(country.others).sort(function(a, b) {
+								if (a.year === b.year)
+									return 0;
+								if (a.year > b.year)
+									return -1;
+								return 1;
+							}).toArray();
+
+							this.dateFormat = "dd MMMM yyyy";
+
+							this.getYearTitle = function(doc) {
+								return doc.dateOfText !== null ? "Date of text: "
+										+ $filter("date")(doc.dateOfText, self.dateFormat)
+										: doc.dateOfOriginalText !== null ? "Date of original text: "
+												+ $filter("date")(doc.dateOfOriginalText, self.dateFormat) : null;
+							};
+
+							this.getYear = function(doc) {
+								return doc.dateOfText !== null ? doc.dateOfText
+										: doc.dateOfOriginalText !== null ? doc.dateOfOriginalText : null;
+							};
+
+							this.docs = Stream(country.faoLexDocuments).sort(function(a, b) {
+								var aYear = self.getYear(a);
+								var bYear = self.getYear(b);
+								if (aYear === bYear)
+									return 0;
+								if (aYear > bYear)
+									return -1;
+								return 1;
+							}).toArray();
+
+							this.flagUrl = imagesservice.countryFlagUrl(country);
+							this.noFlagUrl = imagesservice.missingFlagUrl;
+							this.profileUrl = countryprofiles.profileBaseUrl + country.code + "/en";
+							this.faoLexUrl = faolex.baseUrl + (country.code === "EUR" ? "EC:" : "ISO:")
+									+ country.code;
+
+							this.showPoa = function(poa) {
+								routingservice.toSingle("poas", poa);
+							};
+
+							this.showEntityById = function(acronym) {
+								routingservice.toSingleById("entities", acronym);
+							};
+
+						} ]);
--- sharks-client/app/scripts/controllers/species_details.js
+++ sharks-client/app/scripts/controllers/species_details.js
@@ -3,51 +3,62 @@
 /**
  * @ngdoc function
  * @name sharksClient.controller:SharkSpeciesCtrl
- * @description
- * # SharkSpeciesCtrl
- * Controller of the sharksClient
+ * @description # SharkSpeciesCtrl Controller of the sharksClient
  */
-angular.module("sharksClient")
-  .controller("SpeciesDetailsCtrl", ["routingservice", "imagesservice", "pageservice", 
-                                     "factsheets", "mapViewer", "species",  
-                                          function (routingservice, imagesservice, pageservice, 
-                                        		  factsheets, mapViewer, species) {
-	  
-	  pageservice.setTitle(species.officialNames.en);
-	  pageservice.setDescription("Measures for species "+species.officialNames.en);
-	  
-	  this.species = species;
-	  this.ems = Stream(species.measures)
-		  .sort(function(a, b) {
-		      if (a.year === b.year) return 0;
-		      if (a.year > b.year) return -1;
-		      return 1;
-		   })
-		  .groupBy(function (measure) {
-		      return measure.entityAcronym;
-		  });
-	  
-	  this.factsheetsUrl = species.figisId !== null? factsheets.speciesBaseUrl + species.figisId : null;
-	  
-	  this.showEntity = function(acronym) {
-		  routingservice.toSingleById("entities", acronym);
-	  };
-	  
-	  this.imageUrl = imagesservice.speciesMediumImageUrl;
-	  this.missingImageUrl = imagesservice.speciesMissingMediumImageUrl;
-	  
-	  this.mapUrl = mapViewer.speciesBaseUrl+"?species="+species.alphaCode+"-m&prj=4326";
-	  
-	  if (species.hasDistributionMap) {
-	      FigisMap.draw({ 
-	          context:"FI-species", 
-	          target:"distributionMap", 
-	          mapSize:"L", 
-	          distribution:[{ layer:"fifao:SPECIES_DIST", 
-	            filter:"ALPHACODE='"+species.alphaCode+"'", 
-	            title:"Abramis brama", 
-	            autoZoom:true }], 
-	            legend:"mapSpeciesLegend", 
-	            staticLabels:{ "MAIN LAYERS" : "Species distribution" } });
-	  }
-  }]);
+angular.module("sharksClient").controller(
+		"SpeciesDetailsCtrl",
+		[
+				"routingservice",
+				"imagesservice",
+				"pageservice",
+				"factsheets",
+				"mapViewer",
+				"species",
+				function(routingservice, imagesservice, pageservice, factsheets, mapViewer, species) {
+
+					pageservice.setTitle(species.officialNames.en);
+					pageservice.setDescription("List of conservation and management measures for "
+							+ species.officialNames.en
+							+ " issued by regional fisheries bodies or relevant conventions");
+
+					this.species = species;
+					this.ems = Stream(species.measures).sort(function(a, b) {
+						if (a.year === b.year)
+							return 0;
+						if (a.year > b.year)
+							return -1;
+						return 1;
+					}).groupBy(function(measure) {
+						return measure.entityAcronym;
+					});
+
+					this.factsheetsUrl = species.figisId !== null ? factsheets.speciesBaseUrl
+							+ species.figisId : null;
+
+					this.showEntity = function(acronym) {
+						routingservice.toSingleById("entities", acronym);
+					};
+
+					this.imageUrl = imagesservice.speciesMediumImageUrl;
+					this.missingImageUrl = imagesservice.speciesMissingMediumImageUrl;
+
+					this.mapUrl = mapViewer.speciesBaseUrl + "?species=" + species.alphaCode + "-m&prj=4326";
+
+					if (species.hasDistributionMap) {
+						FigisMap.draw({
+							context : "FI-species",
+							target : "distributionMap",
+							mapSize : "L",
+							distribution : [ {
+								layer : "fifao:SPECIES_DIST",
+								filter : "ALPHACODE='" + species.alphaCode + "'",
+								title : "Abramis brama",
+								autoZoom : true
+							} ],
+							legend : "mapSpeciesLegend",
+							staticLabels : {
+								"MAIN LAYERS" : "Species distribution"
+							}
+						});
+					}
+				} ]);
