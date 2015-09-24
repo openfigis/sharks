ed03955c8c4f1e029248be2181222613d4ecde88
erikvaningen <erikvaningen@gmail.com>
2015-09-24 14:41:17 +0200

    Fixed: But for the meta description of the institutions is still the old, maybe it is better to put:
    <meta name="description" content="List of measures for the conservation
    and management of sharks issued by <institution name> (<acronym>)."> and
    get rid of the rest.

--- sharks-client/app/scripts/controllers/entity_details.js
+++ sharks-client/app/scripts/controllers/entity_details.js
@@ -1,60 +1,57 @@
 "use strict";
 
-angular
-		.module("sharksClient")
-		.controller(
-				"EntityDetailsCtrl",
-				[
-						"routingservice",
-						"pageservice",
-						"entityTypes",
-						"mapViewer",
-						"entity",
-						function(routingservice, pageservice, entityTypes, mapViewer, entity) {
+angular.module("sharksClient").controller(
+		"EntityDetailsCtrl",
+		[
+				"routingservice",
+				"pageservice",
+				"entityTypes",
+				"mapViewer",
+				"entity",
+				function(routingservice, pageservice, entityTypes, mapViewer, entity) {
 
-							pageservice.setTitle(entity.acronym);
-							pageservice
-									.setDescription("List of measures for the conservation and management of sharks issued by " +
-											 entity.acronym + ", a regional fishery body or relevant global convention.");
+					pageservice.setTitle(entity.acronym);
+					pageservice.setDescription("List of measures for the conservation and management of sharks issued by " +
+							 entity.acronym);
 
-							this.entity = entity;
+					this.entity = entity;
 
-							this.ems = Stream(entity.measures).sort(function(a, b) {
-								if (a.year === b.year)
-									return 0;
-								if (a.year > b.year)
-									return -1;
-								return 1;
-							}).toArray();
+					this.ems = Stream(entity.measures).sort(function(a, b) {
+						if (a.year === b.year)
+							return 0;
+						if (a.year > b.year)
+							return -1;
+						return 1;
+					}).toArray();
 
-							this.others = Stream(entity.others).sort(function(a, b) {
-								if (a.year === b.year)
-									return 0;
-								if (a.year > b.year)
-									return -1;
-								return 1;
-							}).toArray();
+					this.others = Stream(entity.others).sort(function(a, b) {
+						if (a.year === b.year)
+							return 0;
+						if (a.year > b.year)
+							return -1;
+						return 1;
+					}).toArray();
 
-							this.hasCompentenceAreaMap = entity.type === entityTypes.rfmo;
+					this.hasCompentenceAreaMap = entity.type === entityTypes.rfmo;
 
-							this.members = Stream(entity.members).sorted("name").toArray();
+					this.members = Stream(entity.members).sorted("name").toArray();
 
-							this.isCites = entity.acronym === "CITES";
+					this.isCites = entity.acronym === "CITES";
 
-							this.showCountry = function(country) {
-								routingservice.toSingle("countries", country);
-							};
+					this.showCountry = function(country) {
+						routingservice.toSingle("countries", country);
+					};
 
-							this.mapUrl = mapViewer.rfbBaseUrl;
+					this.mapUrl = mapViewer.rfbBaseUrl;
 
-							if (this.hasCompentenceAreaMap) {
-								FigisMap.draw({
-									target : "areaMap",
-									rfb : entity.acronym,
-									context : "FI-Org",
-									legend : "OrgMapLegend",
-									mapSize : "S"
-								});
-							}
+					if (this.hasCompentenceAreaMap) {
+						FigisMap.draw({
+							target : "areaMap",
+							rfb : entity.acronym,
+							context : "FI-Org",
+							legend : "OrgMapLegend",
+							mapSize : "S"
+						});
+					}
 
-						} ]);
+				} ]);
