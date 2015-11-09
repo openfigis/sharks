687d7c91b254f7572abe1939d8c65e8041d32798
erikvaningen <erikvaningen@gmail.com>
2015-11-09 15:43:06 +0100

    testing the null

--- sharks-server/sharks-service/src/test/java/org/sharks/service/cache/ServiceCacheProducerTest.java
+++ sharks-server/sharks-service/src/test/java/org/sharks/service/cache/ServiceCacheProducerTest.java
@@ -1,7 +1,9 @@
 package org.sharks.service.cache;
 
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.sharks.service.test.util.TestUtils.getService;
 
 import javax.enterprise.inject.Produces;
@@ -15,32 +17,39 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.sharks.service.Service;
 import org.sharks.service.Service.ServiceType;
+import org.sharks.service.cache.ServiceCacheManager.ServiceInfo;
 
 /**
  * @author "Federico De Faveri federico.defaveri@fao.org"
  *
  */
 @RunWith(CdiRunner.class)
-@AdditionalClasses({ServiceCacheProducer.class})
-@Service(name="myTest",type=ServiceType.INTERNAL)
+@AdditionalClasses({ ServiceCacheProducer.class })
+@Service(name = "myTest", type = ServiceType.INTERNAL)
 public class ServiceCacheProducerTest {
-	
-	@Inject @CacheName("myCache")
+
+	static String cache = "myCache";
+
+	@Inject
+	@CacheName("myCache")
 	ServiceCache<Long, String> namedCache;
-	
+
 	@Inject
 	ServiceCacheManager manager;
-	
-	@Produces @Singleton
+
+	@Produces
+	@Singleton
 	private ServiceCacheManager setupServiceCacheManager() {
 		ServiceCacheManager manager = Mockito.mock(ServiceCacheManager.class);
-
+		when(manager.getOrCreateCache(new ServiceInfo("myTest", ServiceType.INTERNAL), "myCache"))
+				.thenReturn(new InMemoryCache<Object, Object>());
 		return manager;
 	}
 
 	@Test
 	public void test() {
-		verify(manager, times(1)).getOrCreateCache(getService("myTest"),"myCache");
+		assertNotNull(namedCache);
+		verify(manager, times(1)).getOrCreateCache(getService("myTest"), "myCache");
 	}
 
 }
