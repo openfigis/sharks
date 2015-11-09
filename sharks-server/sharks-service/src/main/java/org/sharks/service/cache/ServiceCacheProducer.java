51cc5f090451392b8c829adb233bd13a5a18c4bc
erikvaningen <erikvaningen@gmail.com>
2015-11-09 14:27:22 +0100

    formatting

--- sharks-server/sharks-service/src/main/java/org/sharks/service/cache/ServiceCacheProducer.java
+++ sharks-server/sharks-service/src/main/java/org/sharks/service/cache/ServiceCacheProducer.java
@@ -16,16 +16,22 @@ import org.sharks.service.cache.ServiceCacheManager.ServiceInfo;
  */
 @ApplicationScoped
 public class ServiceCacheProducer {
-	
+
 	@Produces
-	public <K,V> ServiceCache<K, V> getCache(InjectionPoint injectionPoint, ServiceCacheManager cacheManager) {
-		
+	public <K, V> ServiceCache<K, V> getCache(InjectionPoint injectionPoint, ServiceCacheManager cacheManager) {
+
 		Service service = injectionPoint.getMember().getDeclaringClass().getAnnotation(Service.class);
-		if (service == null) throw new IllegalArgumentException("The injection point "+injectionPoint+" is missing the "+Service.class.getSimpleName()+" annotation");
-		
+		if (service == null) {
+			throw new IllegalArgumentException("The injection point " + injectionPoint + " is missing the "
+					+ Service.class.getSimpleName() + " annotation");
+		}
+
 		CacheName cacheNameAnnotation = injectionPoint.getAnnotated().getAnnotation(CacheName.class);
-		if (cacheNameAnnotation == null) throw new IllegalArgumentException("The injection point "+injectionPoint+" is missing the "+CacheName.class.getSimpleName()+" annotation");
-				
+		if (cacheNameAnnotation == null) {
+			throw new IllegalArgumentException("The injection point " + injectionPoint + " is missing the "
+					+ CacheName.class.getSimpleName() + " annotation");
+		}
+
 		String cacheName = cacheNameAnnotation.value();
 
 		return cacheManager.getOrCreateCache(new ServiceInfo(service.name(), service.type()), cacheName);
