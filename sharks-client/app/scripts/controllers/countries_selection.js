fea0aa10db31377aaab329c9f6a73208f8df33c6
erikvaningen <erikvaningen@gmail.com>
2015-12-14 17:23:41 +0100

    moving the Taiwan exclusion from the backend to the frontend

--- sharks-client/app/scripts/controllers/countries_selection.js
+++ sharks-client/app/scripts/controllers/countries_selection.js
@@ -1,27 +1,32 @@
 "use strict";
 
-angular.module("sharksClient")
-  .controller("CountriesSelectionCtrl", ["routingservice", "pageservice", "countries", "footer", 
-                                         function (routingservice, pageservice, countries, footer) {
-	  
-	  this.groupedCountries = Stream(countries)
-	  	.sorted(function(a, b) {
-	  		  if (a.code === "EUR") return 1;
-	  		  if (b.code === "EUR") return -1;
-	  		
-		      if (a.name === b.name) return 0;
-		      if (a.name < b.name) return -1;
-		      return 1;
-		   })
-	  	.groupBy(function (country) {
-	  		if (country.code === "EUR") return "Europe";
-	        return country.continent;
-	    });
-	  
-	  pageservice.setTitle("countries");
-	  this.footer = footer;
+angular.module("sharksClient").controller("CountriesSelectionCtrl",
+    [ "routingservice", "pageservice", "countries", "footer", function(routingservice, pageservice, countries, footer) {
 
-	  this.show = function(country) {
-		  routingservice.toSingle("countries", country);
-	  };
-  }]);
+      this.groupedCountries = Stream(countries).sorted(function(a, b) {
+        if (a.code === "EUR")
+          return 1;
+        if (b.code === "EUR")
+          return -1;
+
+        if (a.name === b.name)
+          return 0;
+        if (a.name < b.name)
+          return -1;
+        return 1;
+      }).filter(function(country) {
+        // Taiwan is not shown in this presentation.
+        return country.code !== "TWN";
+      }).groupBy(function(country) {
+        if (country.code === "EUR")
+          return "Europe";
+        return country.continent;
+      });
+
+      pageservice.setTitle("countries");
+      this.footer = footer;
+
+      this.show = function(country) {
+        routingservice.toSingle("countries", country);
+      };
+    } ]);
