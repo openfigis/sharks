8b1cb7fd300686996b9d47b68f86d66ce026ab9c
erikvaningen <erikvaningen@gmail.com>
2015-12-17 11:38:36 +0100

    excluded Taiwain from the country browse

--- sharks-client/app/scripts/controllers/browse.js
+++ sharks-client/app/scripts/controllers/browse.js
@@ -1,29 +1,33 @@
 "use strict";
 
-angular.module("sharksClient")
-.controller("BrowseCtrl", ["routingservice", "pageservice", "species", "groups", "entities", "countries",
-                                     function (routingservice, pageservice, species, groups, entities, countries) {
-	
-	  pageservice.setTitle("browse");
-	
-	  this.species = Stream(species).sorted("englishName").toArray();
-	  this.groups = Stream(groups).sorted("name").toArray();
-	  this.entities = Stream(entities).sorted("acronym").toArray();
-	  this.countries = Stream(countries).sorted("name").toArray();
-	  
-	  this.showSpecies = function(species) {
-		  routingservice.toSingle("species", species);
-	  };
-	  
-	  this.showGroup = function(group) {
-		  routingservice.toSingle("groups", group);
-	  };
-	  
-	  this.showEntity = function(entity) {
-		  routingservice.toSingle("entities", entity);
-	  };
-	  
-	  this.showCountry = function(country) {
-		  routingservice.toSingle("countries", country);
-	  };
-}]);
+angular.module("sharksClient").controller(
+    "BrowseCtrl",
+    [ "routingservice", "pageservice", "species", "groups", "entities", "countries",
+        function(routingservice, pageservice, species, groups, entities, countries) {
+
+          pageservice.setTitle("browse");
+
+          this.species = Stream(species).sorted("englishName").toArray();
+          this.groups = Stream(groups).sorted("name").toArray();
+          this.entities = Stream(entities).sorted("acronym").toArray();
+
+          this.countries = Stream(countries).filter(function(country) { // Taiwan is not shown in this presentation.
+            return country.code !== "TWN";
+          }).sorted("name").toArray();
+
+          this.showSpecies = function(species) {
+            routingservice.toSingle("species", species);
+          };
+
+          this.showGroup = function(group) {
+            routingservice.toSingle("groups", group);
+          };
+
+          this.showEntity = function(entity) {
+            routingservice.toSingle("entities", entity);
+          };
+
+          this.showCountry = function(country) {
+            routingservice.toSingle("countries", country);
+          };
+        } ]);
